0c876dd57324071001297cb3a69761e5bc1682df
supensa <stitch@mobap.local>
2017-03-12 21:36:38 +1100

    Initial Commit

---
+++ Messager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Messager
+//
+//  Created by Spencer Forrest on 12/03/2017.
+//  Copyright © 2017 Spencer Forrest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
